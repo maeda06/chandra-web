e25369d4314189384b82c39788ef74f43b055919
Maeda Ryusei <maeda06@outlook.jp>
2022-05-12 02:08:03 +0900

    first commit

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>chandra-web</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+		<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;700&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
+</head>
+<body>
+<header>
+  <div class="logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logo/logo_web.png" alt="株式会社chandra"></div>
+    <nav>
+      <ul>
+        <li><a href="#">home</a></li>
+        <li><a href="#">home</a></li>
+        <li><a href="#">home</a></li>
+        <li><a href="#">home</a></li>
+        <li><a href="#">home</a></li>
+      </ul>
+    </nav>
+  </header>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+
+  <section id="main-area">
+    <div class="main-area__text">
+      <h1>スモールビジネスにwebのチカラを</h1>
+      <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/logo/logo_web.png" alt="株式会社chandra"> -->
+      <p>スモールビジネス向けの月額10,000円から始められるホームページ運用サービス</p>
+      <div class="button">
+        <p>お気軽にお問合せください！</p>
+        <a href="#">お問い合わせはコチラ</a>
+      </div>
+    </div>
+    <div class="main-area__image"><img src="<?php echo get_template_directory_uri(); ?>/images/Website Creator-amico.png" alt=""></div>
+  </section>
+
+  <section id="worries">
+    <h1>こんな悩みお持ちではありませんか？</h1>
+    <div class="worries__content-wrap">
+      <div>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/worries1.png" alt="">
+        <p>ホームページが欲しいのに制作費が高くて手出せない...</p>
+      </div>
+      <div>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/worries2.png" alt="">
+        <p>webで集客をしたいが何をしたらいいかわからない</p>
+      </div>
+    </div>
+  </section>
+
+  <section id="solution">
+    <h1>その悩み私たちが解決します</h1>
+    <h1>ホームページの制作から運用まで全てお任せください！</h1>
+    <div class="solution__content-wrap">
+      <div class="solution__content">
+        <div class="solution__content-image"><img src="<?php echo get_template_directory_uri(); ?>/images/Content-amico.png" alt=""></div>
+        <h2>専門的な知識は不要です</h2>
+        <p>制作から更新、管理までホームページにかかわる全ての作業をお任せいただけます。</p>
+      </div>
+      <div class="solution__content">
+        <div class="solution__content-image"><img src="<?php echo get_template_directory_uri(); ?>/images/Growing-amico.png" alt=""></div>
+        <h2>月々10,000円からホームページが持てる</h2>
+        <p>定額制のホームページ運用サービスです。月々10,000円～プランをご用意しています。</p>
+      </div>
+      <div class="solution__content">
+        <div class="solution__content-image"><img src="<?php echo get_template_directory_uri(); ?>/images/Consulting-amico.png" alt=""></div>
+        <h2>お客様専任のウェブ担当者を配属​</h2>
+        <p>集客の強化やブランディング向上など、お客様のご要望に応えるために、専任の担当者を配属します。</p>
+      </div>
+    </div>
+  </section>
+
+  <?php get_footer(); ?>
