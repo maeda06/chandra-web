18422f2d539480a0235ac185cc124f529836b086
Maeda Ryusei <maeda06@outlook.jp>
2022-05-13 19:10:54 +0900

    flowまで

--- index.php
+++ index.php
@@ -32,21 +32,49 @@
     <h1>ホームページの制作から運用まで全てお任せください！</h1>
     <div class="solution__content-wrap">
       <div class="solution__content">
+        <div class="solution__content--text">
+          <h2>専門的な知識は<span class="blue">不要です</span></h2>
+          <p>制作から更新、管理までホームページにかかわる全ての作業をお任せいただけます。</p>
+        </div>
         <div class="solution__content-image"><img src="<?php echo get_template_directory_uri(); ?>/images/Content-amico.png" alt=""></div>
-        <h2>専門的な知識は不要です</h2>
-        <p>制作から更新、管理までホームページにかかわる全ての作業をお任せいただけます。</p>
       </div>
       <div class="solution__content">
+        <div class="solution__content--text">
+          <h2><span class="blue">月々10,000円から</span>ホームページが持てる</h2>
+          <p>定額制のホームページ運用サービスです。月々10,000円～プランをご用意しています。</p>
+        </div>
         <div class="solution__content-image"><img src="<?php echo get_template_directory_uri(); ?>/images/Growing-amico.png" alt=""></div>
-        <h2>月々10,000円からホームページが持てる</h2>
-        <p>定額制のホームページ運用サービスです。月々10,000円～プランをご用意しています。</p>
       </div>
       <div class="solution__content">
+        <div class="solution__content--text">
+          <h2><span class="blue">お客様専任</span>のウェブ担当者を配属​</h2>
+          <p>集客の強化やブランディング向上など、お客様のご要望に応えるために、専任の担当者を配属します。</p>
+        </div>
         <div class="solution__content-image"><img src="<?php echo get_template_directory_uri(); ?>/images/Consulting-amico.png" alt=""></div>
-        <h2>お客様専任のウェブ担当者を配属​</h2>
-        <p>集客の強化やブランディング向上など、お客様のご要望に応えるために、専任の担当者を配属します。</p>
       </div>
     </div>
+  </section>
+
+  <section id="point">
+    <div class="point__content--image"><img src="<?php echo get_template_directory_uri(); ?>/images/point-image.png" alt=""></div>
+    <div class="point__content--text">
+      <span>CHANDRA POINT</span>
+      <h2>CHANDRAの特徴</h2>
+      <p>CHANDRAでは中小企業向けに低価格でホームページを持つことができるように、サブスクリプション型（月額定額制）のホームページ運用サービスを提供しています。</p>
+      <ul>
+        <li>サブスク型にすることで初期費用を抑え低価格でホームページを制作</li>
+        <li>お客様のビジネスの変化に合わせた柔軟な運用を提供</li>
+        <li>不満な点があればいつでも解約OK！</li>
+      </ul>
+      <div class="point__button">
+        <a href="#">料金プランを見る</a>
+      </div>
+    </div>
+  </section>
+
+  <section id="flow">
+    <span>FLOW PRODACTION</span>
+    <h2>ホームページ制作までの流れ</h2>
   </section>
 
   <?php get_footer(); ?>
