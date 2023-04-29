244d2716120e296f885a8730735af69ed46a4cc2
Maeda Ryusei <maeda06@outlook.jp>
2023-04-29 19:44:56 +0900

    デバッグ用のコード削除

--- header.php
+++ header.php
@@ -54,11 +54,6 @@
     <p>お気軽にお問合せください！</p>
     <a href="contact/">お問い合わせはコチラ</a>
   </div>
-  <?php // 現在使用しているテンプレートファイル名を表示
-  global $template; // テンプレートファイルのパスを取得
-  $temp_name = basename($template); // パスの最後の名前（ファイル名）を取得
-  echo '現在使用しているテンプレートファイル：'.$temp_name; // ファイル名の表示
-?>
 </header>
 
   <div class="hamburger">
