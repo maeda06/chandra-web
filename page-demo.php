34c1cb5806be1fee0ca7e3ca002e9b0ed5bd26bb
Maeda Ryusei <maeda06@outlook.jp>
2024-02-09 15:27:01 +0900

    デモページ変更

--- page-demo.php
+++ page-demo.php
@@ -7,62 +7,38 @@
 	</section>
 
 	<section id="lite" class="demo__content">
-	<div class="plan__content-wrap">
-		<div class="plan__content">
-			<div class="plan__title">
-				<h3>カフェデモ</h3>
-			</div>
-			<div class="plan__price">
-				<p>月額<span class="plan__price--span">10,000</span>円</p>
-				<span>（税込み11,000円）</span>
-			</div>
-			<ul>
-				<li>更新代行（月/1回）</li>
-				<li>ページ数1</li>
-				<li>サーバー管理</li>
-				<li>ドメイン取得代行</li>
-				<li>セキュリティ設定</li>
-				<li>スマホ対応</li>
-				<li>サポート</li>
-				<li>お問い合わせフォーム</li>
-			</ul>
-		</div>
-	</div>
-		<div class="demo__img">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/demo-cafe.png" alt="">
-			<div class="service__button">
-				<a href="https://web.chandra-inc.com/demo02/">デモサイトを見る</a>
+		<div class="demo__item">
+			<div class="plan__content">
+				<div class="plan__title">
+					<h3>ライトプランデモ</h3>
+				</div>
+				<div class="demo__img">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/demo-lite.png" alt="">
+				</div>
+				<div class="plan__price">
+					<p>月額<span class="plan__price--span">9,800</span>円</p>
+					<span>（税込み10,780円）</span>
+				</div>
+				<div class="service__button">
+					<a href="https://web.chandra-inc.com/demo02/" target="_blank">デモサイトを見る</a>
+				</div>
 			</div>
 		</div>
-	</section>
-
-	<section id="standard" class="demo__content">
-	<div class="plan__content-wrap">
-		<div class="plan__content">
-			<div class="plan__title">
-				<h3>焼肉店デモ</h3>
-			</div>
-			<div class="plan__price">
-				<p>月額<span class="plan__price--span">30,000</span>円</p>
-				<span>（税込み33,000円）</span>
-			</div>
-			<ul>
-				<li>更新代行（月/3回）</li>
-				<li>ページ数1</li>
-				<li>お知らせ機能</li>
-				<li>サーバー管理</li>
-				<li>ドメイン取得代行</li>
-				<li>セキュリティ設定</li>
-				<li>スマホ対応</li>
-				<li>サポート</li>
-				<li>お問い合わせフォーム</li>
-			</ul>
-		</div>
-	</div>
-		<div class="demo__img">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/demo-yakiniku.png" alt="">
-			<div class="service__button">
-				<a href="https://web.chandra-inc.com/demo01/">デモサイトを見る</a>
+		<div class="demo__item">
+			<div class="plan__content">
+				<div class="plan__title">
+					<h3>スタンダードプランデモ</h3>
+				</div>
+				<div class="demo__img">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/demo-standard.png" alt="">
+				</div>
+				<div class="plan__price">
+					<p>月額<span class="plan__price--span">14,800</span>円</p>
+					<span>（税込み16,280円）</span>
+				</div>
+				<div class="service__button">
+					<a href="https://web.chandra-inc.com/demo01/" target="_blank">デモサイトを見る</a>
+				</div>
 			</div>
 		</div>
 	</section>
